93613127455433238a3164b6fef8e95e704e5f2b
aaivey22 <angelawings22@gmail.com>
2020-06-04 11:53:21 -0400

    set up server and installed packages

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("morgan"); // Specifies status code
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true })); //  Nested objects is set to true 
+app.use(express.json()); // Allows you to send json files to the server
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
