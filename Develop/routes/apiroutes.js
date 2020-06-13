298b589bb1350b830be7724584721cf205d01c1a
aaivey22 <angelawings22@gmail.com>
2020-06-13 19:52:03 -0400

    api getRoute is now working and displaying stats page correctly

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,52 +1,84 @@
-const db = require("../models")
+const db = require("../models/")
+const router = require("express").Router();
 const weight = [];
-// reudce takes the arr of items to sum beginning on item 0
-function getTotal(arr) {
-    return arr.reduce(getSum, 0)
-}
 
-function getSum(accumulator, num) {
-    return accumulator += num;
-}
 
-module.exports = function (app) {
-    app.get("/api/workouts", function (req, res) {
+module.exports = function (router) {
+
+
+    router.get("/api/workouts/range", (req, res) => {
         db.Workout.find({})
+            // parses and sends it as an array of objects to the front end
             .then(dbWorkout => {
-                console.log(dbWorkout)
-                dbWorkout.forEach(function(workout) {
-                    workout.exercises.forEach(function(exercise) {
-                        weight.push(exercise.weight)
-                    })
-                })
-                const filterWeight = weight.filter(function(weight) {
-                    return weight !== undefined;
-                })
-                // console.log(filterWeight)
-                const weightTotal = getTotal(filterWeight)
-                console.log(weightTotal)
-                // console.log(dbWorkout[0].exercises);
-                res.json(dbWorkout); //parses and sends it as an array of objects to the front end
+                res.json(dbWorkout);
             })
             .catch(err => {
                 res.status(400).json(err);
-            });
+            })
+    });
+};
+    //     router.post("/api/workouts", function (req, res) {
+    //         db.Workout.create({})
+    //             .then(function (dbWorkout) {
+    //                 res.json(dbWorkout)
+    //             })
+    //             .catch(err => {
+    //                 res.json(err);
+    //             });
+    //     })
 
-    })
+    //     router.put("/api/workouts/:id", function (req, res) {
+    //         db.Workout.findByIdAndUpdate(req.params.id,)
+    //     })
+    // }
 
-    app.post("/api/workouts", function(req, res) {
-        db.Workout.create({})
-        .then(function(dbWorkout) {
-            res.json(dbWorkout)
-        })
-        .catch(err => {
-            res.json(err);
-          });
-    })
+//     router.post("/api/workouts", (req, res) => {
+//         Workout.create({})
+//             .then(result => {
+//                 res.json(result);
+//             })
+//             .catch(err => {
+//                 res.json(err);
+//             });
+//     });
 
-    app.put("/api/workouts/:id", function(req, res) {
-db.Workout.findByIdAndUpdate(req.params.id, {$push:{exercises:req.body}})
-    })
-}
+//     router.get("/api/workouts", (req, res) => {
+//         var query = {};
+//         if (req.query.workout_id) {
+//             query.WorkoutId = req.query.workout_id;
+//         }
+//         db.Post.findAll({
+//             where: query,
+//             include: [db.Workout]
+//         }).then(function (dbPost) {
+//             res.json(dbPost);
+//         });
+//     });
 
 
+//     router.put("/api/workouts:id", (req, res) => {
+//         Workout.update(
+//             req.body,
+//             {
+//                 where: {
+//                     id: req.body.id
+//                 }
+//             }).then(function (dbPost) {
+//                 res.json(dbPost);
+//             });
+//     });
+// };
+// module.exports = router;
+
+
+// set up post route to create a new empty object and return the id (mongoose automatically creates an id)
+
+// put route that updates item that was just created because it is empty. Receive id in the request params, the req body will contain the required info.
+// once the item is found in the db, we need to set the req-body to the information.
+
+// getall route, query db and return everything (run find method and pass in an empty object)
+
+// getweeklyworkouts route, use find to pass nothing. Use a mongoose method called limit .limit(7)
+// goes to api/workouts/range (see frontend to see where range is targeted)
+
+// delete route targeted by the id which comes from the request params.
